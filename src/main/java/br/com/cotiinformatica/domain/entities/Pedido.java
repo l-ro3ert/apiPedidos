57b81342cd5cea3ca2381afdeb45aefcd27fb15e
l-ro3ert <lro3ert@gmail.com>
2025-07-16 15:21:29 -0300

    feat: serviços da api de pedidos e configuração da pipeline

--- src/main/java/br/com/cotiinformatica/domain/entities/Pedido.java
+++ src/main/java/br/com/cotiinformatica/domain/entities/Pedido.java
@@ -50,7 +50,9 @@ public class Pedido {
 	@Column(name = "descricao_pedido", length = 500)
 	private String descricaoPedido;
 	@Column(name = "ativo", nullable = false, columnDefinition = "BOOLEAN DEFAULT TRUE")
+//	private Boolean ativo = true;
 	private Boolean ativo;
+
 	
 	@Enumerated(EnumType.STRING)
 	@Column(name = "status", nullable = false)
